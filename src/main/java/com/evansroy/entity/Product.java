f6f5b74b1d58d2b188297353266ec565393b6d6b
evansroy <evansroysir@gmail.com>
2023-03-06 21:02:03 +0300

    initial Commit

---
+++ src/main/java/com/evansroy/entity/Product.java
@@ -0,0 +1,32 @@
+package com.evansroy.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    // Constructors, getters and setters, and other methods...
+
+    public Product(Long id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}
+
